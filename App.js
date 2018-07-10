4b481d2f7e8d96cff30b60be88b29870884df075
dmitryame <dmitry@custommode.com>
2018-07-10 10:55:19 -0400

    resetting the project from scratch

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react'
-import { Provider } from 'react-redux'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import store from './app/store' // Import the store
-import Home from './app/components/Home' // Import the component file
-
-export default class App extends Component {
+export default class App extends React.Component {
   render() {
     return (
-      <Provider store={store}>
-        <Home />
-      </Provider>
-    )
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- app/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-
-import reducers from '../app/reducers' // Import the reducer
-
-// Connect our store to the reducers
-export default createStore(reducers, applyMiddleware(thunk))
