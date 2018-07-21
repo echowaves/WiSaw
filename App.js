e7ff342c55fbf7808d6395fa0fc153bc5e07a80a
dmitryame <dmitry@custommode.com>
2018-07-21 16:27:32 -0400

    Home screen wired up

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, } from 'react-navigation'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+import HomeScreen from './src/screens/HomeScreen'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default createStackNavigator({
+	Home: {
+		screen: HomeScreen,
+	},
+})
