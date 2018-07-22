cc2bae5c94a4231b538a7f8978512684f6869e91
dmitryame <dmitry@custommode.com>
2018-07-21 20:43:41 -0400

    adding more navigation

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
+import React, { Component, } from 'react'
 import { createStackNavigator, } from 'react-navigation'
 
 import HomeScreen from './src/screens/HomeScreen'
+import FeedbackScreen from './src/screens/FeedbackScreen'
 
-
-export default createStackNavigator({
+const RootStack = createStackNavigator({
 	Home: {
 		screen: HomeScreen,
 	},
+	Feedback: {
+		screen: FeedbackScreen,
+	},
+},
+{
+	initialRouteName: 'Home',
 })
+
+export default class App extends Component {
+	render() {
+		return <RootStack />
+	}
+}
