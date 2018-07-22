7fd7b2fe441bed8a05a9d1864863a00d46349ed9
dmitryame <dmitry@custommode.com>
2018-07-21 20:56:12 -0400

    added vector-icons

--- android/app/src/main/java/com/echowaves/wisaw/MainApplication.java
+++ android/app/src/main/java/com/echowaves/wisaw/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.echowaves.wisaw.BuildConfig;
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -23,7 +24,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new VectorIconsPackage()
       );
     }
 
