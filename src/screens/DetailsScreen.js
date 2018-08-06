bf424e0ae397220ca72fcfacdaa564b564c2ef7b
dmitryame <dmitry@custommode.com>
2018-08-06 15:01:51 -0400

    very minor

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import React, { Component, } from 'react'
+import { View, Text, } from 'react-native'
 
 class DetailsScreen extends Component {
 	render() {
