36ff8b0031c20253730514bdad9299d34cfe40a7
dmitryame <dmitry@custommode.com>
2018-08-30 19:53:12 -0400

    updating packages

---
+++ src/screens/PhotosList/reducer.test.js
@@ -0,0 +1,14 @@
+
+import { Reducer, } from 'redux-testkit'
+import uut from './reducer'
+
+describe('photosList reducer', () => {
+	it('should have initial state', () => {
+		expect(uut()).toEqual({
+			photos: [],
+			loading: false,
+			errorMessage: '',
+			thumbnailMode: true,
+		})
+	})
+})
