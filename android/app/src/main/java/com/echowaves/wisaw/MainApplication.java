ada4acf151a4658eeaa312cfa7e4f11f82e46534
dmitryame <dmitry@custommode.com>
2018-12-25 12:08:10 -0500

    added RNCamera

--- android/app/src/main/java/com/echowaves/wisaw/MainApplication.java
+++ android/app/src/main/java/com/echowaves/wisaw/MainApplication.java
@@ -3,6 +3,7 @@ package com.echowaves.wisaw;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.rjblopes.opensettings.OpenSettingsPackage;
 import com.reactlibrary.securekeystore.RNSecureKeyStorePackage;
 import com.swmansion.rnscreens.RNScreensPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
             new OpenSettingsPackage(),
             new RNSecureKeyStorePackage(),
             new RNScreensPackage(),
