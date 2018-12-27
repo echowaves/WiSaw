5b8f986486df642104d7a460dda0946c3271f07e
dmitryame <dmitry@custommode.com>
2018-12-27 18:01:43 -0500

    storing photo in the albom

--- src/screens/Camera/index.js
+++ src/screens/Camera/index.js
@@ -1,154 +1,44 @@
-/* eslint-disable no-console */
-/* eslint-disable react/destructuring-assignment */
-import PropTypes from 'prop-types'
 
 import React, { Component, } from 'react'
 import {
 	StyleSheet,
-	View,
-	StatusBar,
-	Dimensions,
+	Text,
 	TouchableOpacity,
+	View,
+	CameraRoll,
 } from 'react-native'
-import Camera from 'react-native-camera'
-import { Icon, } from 'native-base'
-import RNFS from 'react-native-fs'
+import { RNCamera, } from 'react-native-camera'
 
-import moment from 'moment'
+// const moment = require('moment')
 
-import { dirPicutures, } from './dirStorage'
-
-
-const { height, width, } = Dimensions.get('window')
-const orientation = height > width ? 'Portrait' : 'Landscape'
-
-// move the attachment to app folder
-const moveAttachment = async (filePath, newFilepath) => new Promise((resolve, reject) => {
-	RNFS.mkdir(dirPicutures)
-		.then(() => {
-			RNFS.moveFile(filePath, newFilepath)
-				.then(() => {
-					console.log('FILE MOVED', filePath, newFilepath)
-					resolve(true)
-				})
-				.catch(error => {
-					console.log('moveFile error', error)
-					reject(error)
-				})
-		})
-		.catch(err => {
-			console.log('mkdir error', err)
-			reject(err)
-		})
-})
-
-class CameraScreen extends Component {
-	static propTypes = {
-		navigation: PropTypes.object.isRequired,
-	}
-
-	static navigationOptions = {
-		header: null,
-	}
-
-	constructor(props) {
-		super(props)
-		this.state = {
-			orientation,
+export default class CameraScreen extends Component {
+	takePicture = async function () {
+		if (this.camera) {
+			const options = { quality: 0.5, base64: true, }
+			const data = await this.camera.takePictureAsync(options)
+			CameraRoll.saveToCameraRoll(data.uri)
+			console.log(data.uri)
 		}
-	}
-
-
-	componentWillMount() {
-		Dimensions.addEventListener('change', this.handleOrientationChange)
-	}
-
-	componentWillUnmount() {
-		Dimensions.removeEventListener('change', this.handleOrientationChange)
-	}
-
-	handleOrientationChange = dimensions => {
-		const { height, width, } = dimensions.window
-		const orientation = height > width ? 'Portrait' : 'Landscape'
-		this.setState({ orientation, })
-	}
-
-
-	// ************************** Captur and Save Image *************************
-	saveImage = async filePath => {
-		try {
-			// set new image name and filepath
-			const newImageName = `${moment().format('DDMMYY_HHmmSSS')}.jpg`
-			const newFilepath = `${dirPicutures}/${newImageName}`
-			// move and save image to new filepath
-			const imageMoved = await moveAttachment(filePath, newFilepath)
-			console.log('image moved', imageMoved)
-		} catch (error) {
-			console.log(error)
-		}
-	}
-
-	takePicture() {
-		this.camera
-			.capture()
-			.then(data => {
-			// data is an object with the file path
-			// save the file to app  folder
-				this.saveImage(data.path)
-			})
-			.catch(err => {
-				console.error('capture picture error', err)
-			})
 	}
 
 	render() {
 		return (
-			<View style={{ flex: 1, }}>
-				<StatusBar barStyle="light-content" translucent />
-
-				<Camera
-					captureTarget={Camera.constants.CaptureTarget.disk}
-					ref={cam => {
-						this.camera = cam
+			<View style={styles.container}>
+				<RNCamera
+					ref={ref => {
+						this.camera = ref
 					}}
-					style={styles.container}
-					aspect={Camera.constants.Aspect.fill}
-					orientation="auto">
-					<View
-						style={
-							this.state.orientation === 'Portrait' ? (
-								styles.buttonContainerPortrait
-							) : (
-								styles.buttonContainerLandscape
-							)
-						}>
-						<TouchableOpacity
-							onPress={() => this.takePicture()}
-							style={
-								this.state.orientation === 'Portrait' ? (
-									styles.buttonPortrait
-								) : (
-									styles.buttonLandscape
-								)
-							}>
-							<Icon name="camera" style={{ fontSize: 40, color: 'white', }} />
-						</TouchableOpacity>
-						<TouchableOpacity
-							onPress={() => this.props.navigation.goBack()}
-							style={
-								this.state.orientation === 'Portrait' ? (
-									styles.buttonPortrait
-								) : (
-									styles.buttonLandscape
-								)
-							}>
-							<Icon
-								name="close-circle"
-								style={{ fontSize: 40, color: 'white', }}
-							/>
-						</TouchableOpacity>
-					</View>
-				</Camera>
+					style={styles.preview}
+					type={RNCamera.Constants.Type.back}
+					flashMode={RNCamera.Constants.FlashMode.off}
+				/>
+				<View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center', }}>
+					<TouchableOpacity
+						onPress={this.takePicture.bind(this)}
+						style={styles.capture}>
+						<Text style={{ fontSize: 14, }}> SNAP </Text>
+					</TouchableOpacity>
+				</View>
 			</View>
 		)
 	}
@@ -157,35 +47,21 @@ class CameraScreen extends Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-	},
-	buttonContainerPortrait: {
-		position: 'absolute',
-		bottom: 0,
-		left: 0,
-		right: 0,
-		flexDirection: 'row',
-		justifyContent: 'center',
-		backgroundColor: 'rgba(0, 0, 0, 0.9)',
-	},
-	buttonContainerLandscape: {
-		position: 'absolute',
-		bottom: 0,
-		top: 0,
-		right: 0,
 		flexDirection: 'column',
-		justifyContent: 'center',
-		backgroundColor: 'rgba(0, 0, 0, 0.5)',
+		backgroundColor: 'black',
 	},
-	buttonPortrait: {
-		backgroundColor: 'transparent',
-		padding: 5,
-		marginHorizontal: 20,
+	preview: {
+		flex: 1,
+		justifyContent: 'flex-end',
+		alignItems: 'center',
 	},
-	buttonLandscape: {
-		backgroundColor: 'transparent',
-		padding: 5,
-		marginVertical: 20,
+	capture: {
+		flex: 0,
+		backgroundColor: '#fff',
+		borderRadius: 5,
+		padding: 15,
+		paddingHorizontal: 20,
+		alignSelf: 'center',
+		margin: 20,
 	},
 })
-
-export default CameraScreen
