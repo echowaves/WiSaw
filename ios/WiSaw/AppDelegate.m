64e910e2d9b0e555f88d311f1646176600c025b7
dmitryame <dmitry@custommode.com>
2018-12-30 14:36:51 -0500

    preparing for ios bundling

--- ios/WiSaw/AppDelegate.m
+++ ios/WiSaw/AppDelegate.m
@@ -16,8 +16,9 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-
+ jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  // jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+    
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"WiSaw"
                                                initialProperties:nil
