2abbed358083e03acbbc2027bdd73651a094ea62
dmitryame <dmitry@custommode.com>
2019-01-07 19:42:09 -0500

    thumb load indicator

--- src/components/Photo/index.js
+++ src/components/Photo/index.js
@@ -5,11 +5,20 @@ import React, {
 import {
 	Dimensions,
 	View,
+	TouchableOpacity,
+	Image,
 } from 'react-native'
+
+import {
+	Icon,
+	Spinner,
+} from 'native-base'
 
 import PhotoView from 'react-native-photo-view-ex'
 
 import PropTypes from 'prop-types'
+
+import * as CONST from '../../consts.js'
 
 class Photo extends Component {
 	static propTypes = {
@@ -26,13 +35,12 @@ class Photo extends Component {
 
 
 		return (
-			<View>
+			<View style={{ flex: 1, }}>
 				<PhotoView
 					source={{
 						uri: item.getImgUrl,
 					}}
 					onLoadEnd={() => this.setState({ loaded: true, })}
-
 					minimumZoomScale={1}
 					maximumZoomScale={5}
 					androidScaleType="fitCenter"
@@ -42,21 +50,44 @@ class Photo extends Component {
 					}}
 					backgroundColor="transparent"
 				/>
-				{!this.state.loaded && ( // eslint-disable-line react/destructuring-assignment
-					<PhotoView
-						source={{
-							uri: item.getThumbUrl,
-						}}
-						minimumZoomScale={1}
-						maximumZoomScale={5}
-						androidScaleType="fitCenter"
-						style={{
-							width,
-							height,
-							position: 'absolute',
-						}}
-						backgroundColor="transparent"
-					/>
+				{ !this.state.loaded && ( // eslint-disable-line react/destructuring-assignment
+					<View style={{
+						flex: 1,
+						position: 'absolute',
+						top: 0,
+						bottom: 0,
+						right: 0,
+						left: 0,
+					}}>
+						<PhotoView
+							source={{
+								uri: item.getThumbUrl,
+							}}
+							minimumZoomScale={1}
+							maximumZoomScale={5}
+							androidScaleType="fitCenter"
+							style={{
+								width,
+								height,
+							}}
+							backgroundColor="transparent"
+						/>
+						<Spinner
+							style={{
+								flex: 1,
+								width,
+								height,
+								position: 'absolute',
+								top: 0,
+								bottom: 0,
+								right: 0,
+								left: 0,
+							}}
+							color={
+								CONST.MAIN_COLOR
+							}
+						/>
+					</View>
 				)}
 			</View>
 		)
