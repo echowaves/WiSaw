4140046042abd53458f8c846744aef2d8811cc7f
dmitryame <dmitry@custommode.com>
2019-01-16 20:13:24 -0500

    ios fabric

--- ios/WiSaw/AppDelegate.m
+++ ios/WiSaw/AppDelegate.m
@@ -7,6 +7,9 @@
 
 #import "AppDelegate.h"
 
+#import <Fabric/Fabric.h>
+#import <Crashlytics/Crashlytics.h>
+
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
@@ -36,6 +39,9 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  [Fabric with:@[[Crashlytics class]]];
+  
   return YES;
 }
 
