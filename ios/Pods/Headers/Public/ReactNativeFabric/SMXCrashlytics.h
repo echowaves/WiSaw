0ff712e11f9697cc0fe64e16c1f21d69e06ef0a5
dmitryame <dmitry@custommode.com>
2019-10-13 21:45:06 -0400

    more deps updates

--- ios/Pods/Headers/Public/ReactNativeFabric/SMXCrashlytics.h
+++
@@ -1,1 +0,0 @@
-../../../../../node_modules/react-native-fabric/ios/SMXCrashlytics/SMXCrashlytics.h
--- ios/Pods/Target Support Files/ReactNativeFabric/ReactNativeFabric-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ReactNativeFabric : NSObject
-@end
-@implementation PodsDummy_ReactNativeFabric
-@end
