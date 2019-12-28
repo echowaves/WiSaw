f6b6a346e546cd7687a7f981e3de2517ca44b0e6
dmitryame <dmitry@custommode.com>
2019-12-28 11:28:52 -0500

    enabled hermes

--- android/app/src/main/java/com/echowaves/wisaw/MainApplication.java
+++ android/app/src/main/java/com/echowaves/wisaw/MainApplication.java
@@ -1,15 +1,15 @@
 package com.echowaves.wisaw;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.PackageList;
-import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
-import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import java.lang.reflect.InvocationTargetException;
+import android.content.Context;
+
 // import Branch and RNBranch
 import io.branch.referral.Branch;
 
@@ -47,5 +47,31 @@ public class MainApplication extends Application implements ReactApplication {
 		// Branch object initialization
 		Branch.getAutoInstance(this);
     SoLoader.init(this, /* native exopackage */ false);
+		initializeFlipper(this); // Remove this line if you don't want Flipper enabled
   }
+	/**
+	* Loads Flipper in React Native templates.
+	*
+	* @param context
+	*/
+ private static void initializeFlipper(Context context) {
+	 if (BuildConfig.DEBUG) {
+		 try {
+			 /*
+				We use reflection here to pick up the class that initializes Flipper,
+			 since Flipper library is not available in release mode
+			 */
+			 Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
+			 aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
+		 } catch (ClassNotFoundException e) {
+			 e.printStackTrace();
+		 } catch (NoSuchMethodException e) {
+			 e.printStackTrace();
+		 } catch (IllegalAccessException e) {
+			 e.printStackTrace();
+		 } catch (InvocationTargetException e) {
+			 e.printStackTrace();
+		 }
+	 }
+ }
 }
