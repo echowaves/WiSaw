9c673a6d96d4fa1e9f1697be97e1e69086d26b91
dmitryame <dmitry@custommode.com>
2021-01-10 09:17:56 -0500

    optimizing image downloading by staggering it a bit

--- src/components/CachedImage/index.js
+++ src/components/CachedImage/index.js
@@ -15,6 +15,9 @@ const CachedImage = props => {
   const componentIsMounted = useRef(true)
 
   useEffect(() => {
+    if (typeof CachedImage.counter === 'undefined') {
+      CachedImage.counter = 0
+    }
     getImageUri()
     return () => {
       componentIsMounted.current = false
@@ -22,30 +25,31 @@ const CachedImage = props => {
   }, [])// eslint-disable-line react-hooks/exhaustive-deps
 
   const getImageUri = async () => {
+    CachedImage.counter += 1
     // console.log(cacheKey)
     try {
       // Use the cached image if it exists
       const metadata = await FileSystem.getInfoAsync(filesystemURI)
       if (!metadata.exists) {
         // download to cache
-
         if (componentIsMounted.current) {
+          // console.log(CachedImage.counter)
           setImgURI(null)
+          await new Promise(r => setTimeout(r, 10 * CachedImage.counter))
           await FileSystem.downloadAsync(
             uri,
             filesystemURI
           )
         }
-        await new Promise(r => setTimeout(r, 100))
+        // await new Promise(r => setTimeout(r, 100))
         if (componentIsMounted.current) {
           setImgURI(filesystemURI)
         }
-        // TODO: ugly timeout
-        // setTimeout(() => { setImgURI(filesystemURI) }, 100)
       }
     } catch (err) {
       setImgURI(uri)
     }
+    CachedImage.counter = 0
   }
 
   return (
