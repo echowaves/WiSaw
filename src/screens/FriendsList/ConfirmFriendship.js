7092ce0d83619479c6ed51c6a4a5dcb3f694d39b
dmitryame <dmitry@custommode.com>
2021-12-17 22:20:08 -0500

    confirm friend workks

--- src/screens/FriendsList/ConfirmFriendship.js
+++ src/screens/FriendsList/ConfirmFriendship.js
@@ -39,6 +39,8 @@ import { Col, Row, Grid } from "react-native-easy-grid"
 
 import PropTypes from 'prop-types'
 
+import LocalContacts from '../../components/LocalContacts'
+
 import * as CONST from '../../consts.js'
 
 import * as reducer from './reducer'
@@ -58,7 +60,9 @@ const ConfirmFriendship = ({ route }) => {
   const [searchTerm, setSearchTerm] = useState('')
   const [contacts, setContacts] = useState([])
 
-  const [contact, setContact] = useState(null)
+  const [showLocalContacts, setShowLocalContacts] = useState(true)
+
+  // const [contact, setContact] = useState(null)
 
   // const topOffset = useSelector(state => state.photosList.topOffset)
 
@@ -108,6 +112,7 @@ const ConfirmFriendship = ({ route }) => {
       }
     />
   )
+
   const _checkPermission = async () => {
     if (!permissionGranted) {
       const { status } = await Contacts.requestPermissionsAsync()
@@ -130,31 +135,15 @@ const ConfirmFriendship = ({ route }) => {
     }
   }
 
-  const _submitSearch = async searchString => {
-    if (searchString.length === 0) {
-      return
-    }
-    // console.log('--------------------------------------------------')
-    // console.log({ searchString })
-    const { data } = await Contacts.getContactsAsync({
-      name: searchString,
-      fields: [/* Contacts.Fields.Emails, */Contacts.Fields.PhoneNumbers],
-    })
-    // console.log({ data })
-    // console.log(data.length)
-    setContacts(data.sort((a, b) => a.name.toLowerCase() > b.name.toLowerCase()))
-  }
-
-  const _confirmFriendship = async ({ uuid, contact }) => {
+  const setContactId = async contactId => {
     try {
-      // const { friendship, chat, chatUser } =
-      // await
       await friendsHelper.confirmFriendship({ friendshipUuid, uuid })
-      await friendsHelper.addFriendshipLocally({ friendshipUuid, contactId: contact.id })
-
-      dispatch(reducer.reloadListOfFriends({ uuid }))
+      await friendsHelper.addFriendshipLocally({ friendshipUuid, contactId })
+      await dispatch(reducer.reloadListOfFriends({ uuid }))
+      await navigation.popToTop()
+      await navigation.navigate('FriendsList')
     } catch (err) {
-      // console.log({ err })
+    // console.log({ err })
       Toast.show({
         text1: 'Unable to confirm Friendship',
         text2: err.toString(),
@@ -166,117 +155,12 @@ const ConfirmFriendship = ({ route }) => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView
-        contentContainerStyle={styles.scrollView}
-        showsVerticalScrollIndicator={
-          false
-        }>
-        <Text>
-          Find your friend in contacts, and click on the name to confirm the friendship.
-        </Text>
-        <View style={{
-          flexDirection: 'row',
-          backgroundColor: CONST.NAV_COLOR,
-        }}>
-          <SearchBar
-            placeholder="Type Contact Name..."
-            placeholderTextColor={CONST.PLACEHOLDER_TEXT_COLOR}
-            onChangeText={currentTerm => {
-              setSearchTerm(currentTerm)
-              _submitSearch(currentTerm)
-            }}
-            value={searchTerm}
-            // onSubmitEditing={
-            //   () => _submitSearch()
-            // }
-            autoFocus
-            containerStyle={{
-              width,
-            }}
-            style={
-              {
-                color: CONST.MAIN_COLOR,
-                backgroundColor: "white",
-                paddingLeft: 10,
-                paddingRight: 10,
-              }
-            }
-            rightIconContainerStyle={{
-              margin: 10,
-            }}
-            lightTheme
-          />
-        </View>
-        {
-          contacts.map((contact, index) => (
-            <ListItem
-              bottomDivider
-              key={contact.id}
-              style={{
-                paddingBottom: 5,
-                paddingLeft: 10,
-                paddingRight: 10,
-                width: '100%',
-              }}
-              onPress={async () => {
-                await _confirmFriendship({ uuid, contact })
-                await navigation.popToTop()
-                await navigation.navigate('FriendsList')
-              }}>
-              <ListItem.Content>
-                <ListItem.Title>{contact.name}</ListItem.Title>
-                {/* <ListItem.Subtitle>{JSON.stringify(contact.emails)}{JSON.stringify(contact.phoneNumbers)}</ListItem.Subtitle> */}
-              </ListItem.Content>
-              <ListItem.Chevron size={40} color={CONST.MAIN_COLOR} />
-            </ListItem>
-
-          ))
-        }
-        <TouchableOpacity
-          style={{
-            flex: 1,
-            flexDirection: 'row',
-          }}
-          onPress={
-            async () => {
-              await Contacts.presentFormAsync(null, {}, {
-                isNew: true,
-                allowsActions: false,
-              })
-              _submitSearch(searchTerm)
-            }
-          }>
-          <Col
-            size={1}
-          />
-          <Col
-            size={10}
-            style={{ flex: 5, justifyContent: "center", alignItems: "center" }}>
-            <Text
-              style={{
-                fontSize: 25,
-                color: CONST.MAIN_COLOR,
-              }}>
-              Or Add new contact to Phone book
-            </Text>
-          </Col>
-          <Col
-            size={1}>
-            <Ionicons
-              name="add-circle"
-              style={
-                {
-                  fontSize: 45,
-                  color: CONST.MAIN_COLOR,
-                }
-              }
-            />
-          </Col>
-        </TouchableOpacity>
-
-      </ScrollView>
+      <LocalContacts
+        show={showLocalContacts}
+        setShow={setShowLocalContacts}
+        setContactId={setContactId}
+      />
     </SafeAreaView>
-
   )
 }
 
