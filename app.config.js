ac7a3292569454b3b32e3b559b25d99c783964cd
dmitryame <dmitry@custommode.com>
2022-01-08 18:10:01 -0500

    preparing new release 7.1.0

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 export default {
   name: "WiSaw",
-  version: "7.0.4",
+  version: "7.1.0",
   extra: {
     API_URI: process.env.API_URI,
     API_KEY: process.env.API_KEY,
@@ -33,7 +33,7 @@ export default {
   ],
   ios: {
     bundleIdentifier: "com.echowaves",
-    buildNumber: "226",
+    buildNumber: "227",
     supportsTablet: true,
     infoPlist: {
       UIBackgroundModes: [
@@ -64,7 +64,7 @@ export default {
       backgroundColor: "#FFFFFF",
     },
     package: "com.echowaves.wisaw",
-    versionCode: 226,
+    versionCode: 227,
     permissions: [
       "INTERNET",
       "SYSTEM_ALERT_WINDOW",
