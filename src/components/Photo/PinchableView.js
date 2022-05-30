5d4b98357e32e2713220024450534bc84a0d9a33
dmitryame <dmitry@custommode.com>
2022-05-30 13:25:12 -0400

    added double tap to rcenter

--- src/components/Photo/PinchableView.js
+++ src/components/Photo/PinchableView.js
@@ -6,11 +6,13 @@ import {
 } from 'react-native'
 import PropTypes from 'prop-types'
 
-import { PinchGestureHandler, State } from 'react-native-gesture-handler'
+import { PinchGestureHandler, TapGestureHandler, State } from 'react-native-gesture-handler'
 import CachedImage from 'expo-cached-image'
 
 const PinchableView = ({ width, height, photo }) => {
   const [scale, setScale] = useState(new Animated.Value(1))
+  const [_touchX, _setTouchX] = useState(0)
+  const [_touchY, _setTouchY] = useState(0)
 
   const onPinchEvent = Animated.event(
     [
@@ -19,19 +21,33 @@ const PinchableView = ({ width, height, photo }) => {
       },
     ],
     {
-      useNativeDriver: true,
+      useNativeDriver: false,
     }
   )
 
   const onPinchStateChange = event => {
     if (event.nativeEvent.oldState === State.ACTIVE) {
-    //   console.log({ event })
+      const newScale = event.nativeEvent.scale < 1 ? new Animated.Value(1) : new Animated.Value(event.nativeEvent.scale)
+      //   console.log({ event })
       Animated.spring(scale, {
-        toValue: event.nativeEvent.scale < 1 ? new Animated.Value(1) : event.nativeEvent.scale,
+        toValue: newScale,
         useNativeDriver: true,
       }).start()
+      setScale(newScale)
     }
   }
+
+  const onDoubleTapEvent = event => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+    //   { console.log({ event }) }
+      if (width / 2 > event.nativeEvent.absoluteX) _setTouchX(_touchX + 50)
+      else _setTouchX(_touchX - 50)
+
+      if (height / 2 > event.nativeEvent.absoluteY) _setTouchY(_touchY + 50)
+      else _setTouchY(_touchY - 50)
+    }
+  }
+
   const styles = StyleSheet.create({
     photoContainer: {
       width,
@@ -46,35 +62,53 @@ const PinchableView = ({ width, height, photo }) => {
   })
 
   return (
-    <PinchGestureHandler
-    //   waitFor={100}
-      onGestureEvent={onPinchEvent}
-      onHandlerStateChange={onPinchStateChange}>
-      <Animated.View
-        style={{
-          width,
-          height,
-          transform: [{ scale }],
-        }}
-        resizeMode="contain">
-        <CachedImage
-          source={{ uri: `${photo.thumbUrl}` }}
-          cacheKey={`${photo.id}-thumb`}
-          resizeMode="contain"
-          style={
-            styles.photoContainer
-          }
-        />
-        <CachedImage
-          source={{ uri: `${photo.imgUrl}` }}
-          cacheKey={`${photo.id}`}
-          resizeMode="contain"
-          style={
-            styles.photoContainer
-          }
-        />
-      </Animated.View>
-    </PinchGestureHandler>
+    <TapGestureHandler
+      onHandlerStateChange={onDoubleTapEvent}
+      numberOfTaps={2}>
+      <PinchGestureHandler
+        //   waitFor={100}
+        onGestureEvent={onPinchEvent}
+        onHandlerStateChange={onPinchStateChange}>
+        <Animated.View
+          style={{
+            width,
+            height,
+            transform: [
+              { scale },
+              {
+                translateX: Animated.add(
+                  _touchX,
+                  new Animated.Value(0),
+                ),
+              },
+              {
+                translateY: Animated.add(
+                  _touchY,
+                  new Animated.Value(0),
+                ),
+              },
+            ],
+          }}
+          resizeMode="contain">
+          <CachedImage
+            source={{ uri: `${photo.thumbUrl}` }}
+            cacheKey={`${photo.id}-thumb`}
+            resizeMode="contain"
+            style={
+              styles.photoContainer
+            }
+          />
+          <CachedImage
+            source={{ uri: `${photo.imgUrl}` }}
+            cacheKey={`${photo.id}`}
+            resizeMode="contain"
+            style={
+              styles.photoContainer
+            }
+          />
+        </Animated.View>
+      </PinchGestureHandler>
+    </TapGestureHandler>
   )
 }
 
