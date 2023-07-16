80606854b0edbb7e29a26ca789762f0dd07e7552
dmitryame <dmitry@custommode.com>
2023-07-16 19:56:49 -0400

    build 269

--- app.config.js
+++ app.config.js
@@ -38,7 +38,7 @@ export default {
       // jsEngine: 'jsc',
       jsEngine: 'hermes',
       bundleIdentifier: 'com.echowaves',
-      buildNumber: '268',
+      buildNumber: '269',
       supportsTablet: true,
       infoPlist: {
         UIBackgroundModes: ['fetch'],
@@ -73,7 +73,7 @@ export default {
         backgroundColor: '#FFFFFF',
       },
       package: 'com.echowaves.wisaw',
-      versionCode: 268,
+      versionCode: 269,
       permissions: [
         'INTERNET',
         'SYSTEM_ALERT_WINDOW',
