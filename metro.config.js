be49b782a7ef8ac76410abc1e215aa47f775750e
dmitryame <dmitry@custommode.com>
2025-06-10 18:49:33 -0400

    reverted metroconfig

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,18 @@
-const { getDefaultConfig } = require('expo/metro-config')
+const { getDefaultConfig } = require('@expo/metro-config')
 
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname, {
+  // Enable CSS support
+  isCSSEnabled: true,
+})
 
-config.resolver.sourceExts.push('ios.js', 'android.js')
+// const { resolver: defaultResolver } = config
+// exports.resolver = {
+//   ...defaultResolver,
+//   sourceExts: [
+//     ...defaultResolver.sourceExts, // 'js', 'json', 'ts', 'tsx',
+//     "cjs",
+//   ],
+// }
+config.resolver.sourceExts.push('cjs')
 
 module.exports = config
