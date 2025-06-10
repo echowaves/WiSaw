df9464540167d0ba90514099995403d16a812fef
dmitryame <dmitry@custommode.com>
2025-06-10 18:40:42 -0400

    fixing metroconfig

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,3 @@
 const { getDefaultConfig } = require('@expo/metro-config')
 
-// Get the default Expo Metro config
-const config = getDefaultConfig(__dirname, {
-  // Enable CSS support
-  isCSSEnabled: true,
-})
-
-// Add 'cjs' to source extensions
-config.resolver.sourceExts.push('cjs')
-
-module.exports = config
+module.exports = getDefaultConfig(__dirname)
