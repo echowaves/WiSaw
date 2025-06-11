8a226aae93f5adc463b42230d0ed94c52d8e89df
dmitryame <dmitry@custommode.com>
2025-06-11 09:59:10 -0400

    build 285

--- app.config.js
+++ app.config.js
@@ -43,7 +43,7 @@ export default {
       // jsEngine: 'jsc',
       // jsEngine: 'hermes',
       bundleIdentifier: 'com.echowaves',
-      buildNumber: '284',
+      buildNumber: '285',
       supportsTablet: true,
       infoPlist: {
         UIBackgroundModes: ['processing'],
@@ -77,7 +77,7 @@ export default {
         backgroundColor: '#FFFFFF',
       },
       package: 'com.echowaves.wisaw',
-      versionCode: 284,
+      versionCode: 285,
       permissions: [
         'INTERNET',
         'SYSTEM_ALERT_WINDOW',
