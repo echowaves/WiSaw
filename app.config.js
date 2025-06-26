6a69cf47b58304dfc1c5e8c109ed206613b574dd
dmitryame <dmitry@custommode.com>
2025-06-26 14:25:29 -0400

    feat: update app version to 7.2.6 and build number to 338

--- app.config.js
+++ app.config.js
@@ -7,7 +7,7 @@ module.exports = {
     jsEngine: 'hermes',
     updates: { url: 'https://u.expo.dev/d1624159-fed7-42bf-b11b-7ea8f38a8dee' },
     name: 'WiSaw',
-    version: '7.2.5',
+    version: '7.2.6',
     plugins: [
       // Branch removed - using native deep linking instead
       'expo-secure-store',
@@ -47,7 +47,7 @@ module.exports = {
     },
     ios: {
       bundleIdentifier: 'com.echowaves',
-      buildNumber: '337',
+      buildNumber: '338',
       supportsTablet: true,
       infoPlist: {
         LSApplicationQueriesSchemes: [
@@ -98,7 +98,7 @@ module.exports = {
         backgroundColor: '#FFFFFF',
       },
       package: 'com.echowaves.wisaw',
-      versionCode: 337,
+      versionCode: 338,
       permissions: [
         'INTERNET',
         'SYSTEM_ALERT_WINDOW',
